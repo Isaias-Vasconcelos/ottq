834f8aa8aa133ed4707ac8230806f4b105cacce9
Isaias Vasconcelos <isaiaskeyboards@gmail.com>
2024-04-18 01:05:12 -0300

    First Commit

---
+++ OTTQ/TQ.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace OTTQ
+{
+    public static class TQ<T>
+    {
+        public static string SqlCreateTable(T tableObject)
+        {
+            var type = tableObject.GetType();
+            var tableName = type.Name;
+
+            var query = $@"CREATE TABLE IF NOT EXISTS {tableName.ToLower()} (";
+
+            PropertyInfo[] propiedades = type.GetProperties();
+
+            foreach (var prop in propiedades)
+            {
+                var columnName = prop.Name.ToLower();
+
+                var typeColumn = prop.GetValue(tableObject);
+
+                query += $"{columnName} {typeColumn},";
+            }
+
+            query += $"PRIMARY KEY(id));";
+
+            return query ;
+        }
+    }
+}
